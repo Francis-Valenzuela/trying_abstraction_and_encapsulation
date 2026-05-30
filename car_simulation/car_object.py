0e84cea30ddf8a68ac0acaecdd7da3e53cecc6e5
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-05-30 15:07:47 +0800

    adding exception handling once user fail to answer a number for the year model

--- car_simulation/car_object.py
+++ car_simulation/car_object.py
@@ -1,5 +1,12 @@
 from car_info import Car
-year_model = int(input("Enter a Year Model of Car:"))
-model = input("Enter a model of Car:")
+while True:
+    try:
+        year_model = int(input("Enter a Year Model of Car:"))
+        model = input("Enter a model of Car:")
+        break
+
+    except ValueError:
+        print("Please enter a year model of Car")
 
 my_car = Car(year_model,model)
+print(my_car)
