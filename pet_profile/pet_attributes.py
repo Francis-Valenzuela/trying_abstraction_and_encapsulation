9a55816e927104945e240862f4e840775bae2233
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-05-30 15:59:51 +0800

    creating an object called my_pet and uses the imported method from pet_record.py

--- pet_profile/pet_attributes.py
+++ pet_profile/pet_attributes.py
@@ -3,3 +3,6 @@ from pet_record import Pet
 name = input("Name of the Pet:")
 type_of_animal = input("Type of Animal:")
 pet_age = input("Age of the Pet:")
+
+my_pet = Pet(name, type_of_animal, pet_age)
+print(my_pet)
