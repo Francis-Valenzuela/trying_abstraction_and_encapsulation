cc270a8733080a8de48ac8a71a24d80195e56923
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-05-30 14:55:40 +0800

    letting user to input the year model and the model itself

--- car_simulation/car_object.py
+++ car_simulation/car_object.py
@@ -1,3 +1,5 @@
 from car_info import Car
+year_model = int(input("Enter a Year Model of Car:"))
+model = input("Enter a model of Car:")
 
-my_car = Car("Lamborghini", 2024)
+my_car = Car(year_model,model)
