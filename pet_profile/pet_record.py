f39478eb8d415ef459b08805ca5ff3a20397d654
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-05-30 15:51:16 +0800

    creating a get_name where it returns to self.__name and this will be overwritten through the setters and this getters will be use in printing

--- pet_profile/pet_record.py
+++ pet_profile/pet_record.py
@@ -3,10 +3,14 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
-
+    # setters
     def set_name(self, name):
         self.__name = name
     def set_animal_type(self, animal_type):
         self.__animal_type = animal_type
     def set_age(self, age):
         self.__age = age
+
+    #getters
+    def get_name(self):
+        return self.__name
