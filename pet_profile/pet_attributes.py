d2942c171e7a4d8625f617a2a3f79b4ada4a9a51
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-05-30 16:03:36 +0800

    adding new line to seperate the asking of input and the information itself

--- pet_profile/pet_attributes.py
+++ pet_profile/pet_attributes.py
@@ -6,7 +6,7 @@ pet_age = input("Age of the Pet:")
 
 my_pet = Pet(name, type_of_animal, pet_age)
 
-print("Pet Information:")
+print("\nPet Information")
 print("Name:", my_pet.get_name())
 print("Animal Type:", my_pet.get_animal_type())
 print("Age:", my_pet.get_age())
